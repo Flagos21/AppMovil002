161e19669d49c96cbd51e854990043c670e107cf
Flagos21 <flagosp21@gmail.com>
2024-04-09 17:14:38 -0400

    Commit MainActivity code

--- app/src/main/java/cl/isisur/appmovil002/MainActivity.java
+++ app/src/main/java/cl/isisur/appmovil002/MainActivity.java
@@ -1,24 +1,37 @@
 package cl.isisur.appmovil002;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout layoutPrincipal;
+    private TextView textoPalta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        layoutPrincipal = findViewById(R.id.idPalta);
+        textoPalta = findViewById(R.id.texto);
+
+        Button botonTomate = findViewById(R.id.botonTomate);
+        botonTomate.setOnClickListener(view -> {
+            cambiarColorTexto(Color.parseColor("#E4766E"), "Tomate");
         });
+        Button botonPepino = findViewById(R.id.botonPepino);
+        botonPepino.setOnClickListener(view -> {
+            cambiarColorTexto(Color.parseColor("#9DDA56"), "PEPINO");
+        });
+    }
+
+    private void cambiarColorTexto(int colorFondo, String nuevoTexto) {
+        layoutPrincipal.setBackgroundColor(colorFondo);
+        textoPalta.setText(nuevoTexto);
     }
 }
